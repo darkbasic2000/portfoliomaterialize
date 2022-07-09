cd4983bf44577a4491ce57b0083ebe9d90e12a7c
darkbasic2000 <tecnologo.tcn2018@gmail.com>
2022-07-08 21:18:35 -0400

    Página inicial do projeto, apenas para fazer o redirecionamento para desktop ou mobile

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo "Itemizze - Index"; ?></title>
+    </head>
+    
+    <body>
+        
+        <script src="/js/mobile.js"></script>
+       
+    </body>
+    
+</html>
